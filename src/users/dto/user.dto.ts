7211ee2599e9288c80c904964edbe271748c81f1
Lleroc <llleroc@yahoo.es>
2024-03-14 20:52:18 -0500

    inicial

---
+++ src/users/dto/user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class UserDTO {
+  @IsNotEmpty({ message: 'El texto es requerido' })
+  @IsString()
+  readonly name: string;
+  @IsNotEmpty({ message: 'El texto es requerido' })
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty({ message: 'El texto es requerido' })
+  @IsString()
+  readonly password: string;
+  @IsNotEmpty({ message: 'El texto es requerido' })
+  @IsString()
+  readonly username: string;
+}
