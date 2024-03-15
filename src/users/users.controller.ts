881786186acf1354ae0c25fab0c7210ec23e2628
Lleroc <llleroc@yahoo.es>
2024-03-14 21:40:04 -0500

    user_controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDTO } from './dto/user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/v2/users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  insertar(@Body() userDTO: UserDTO) {
+    return this.userService.insertar(userDTO);
+  }
+}
